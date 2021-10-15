b550e12a21957596217be3e081d37c551d3b8175
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-15 14:02:22 -0300

    Fix download scripts.

--- .github/workflows/scripts/download_exaplus.sh
+++ .github/workflows/scripts/download_exaplus.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-mkdir -p downloads/EXAPLUS
+mkdir -p "$1/downloads/EXAPLUS"
 curl -s https://www.exasol.com/support/secure/attachment/155319/EXAplus-7.0.11.tar.gz | tar -C "$1/downloads/EXAPLUS" --strip-components 1 -zxf -
