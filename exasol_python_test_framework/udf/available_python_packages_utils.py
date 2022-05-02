3c9c85bcce009a43aca53579765599ba219f8059
tkilias <torsten.kilias@exasol.com>
2022-05-02 10:22:36 +0200

    Add schema parameter to available_python_packages_utils.py

--- exasol_python_test_framework/udf/available_python_packages_utils.py
+++ exasol_python_test_framework/udf/available_python_packages_utils.py
@@ -1,22 +1,22 @@
 import textwrap
 
-def run_python_package_import_test(test_case, pkg, language_alias, fail=False, alternative=None):
-    sql=textwrap.dedent('''
-        CREATE OR REPLACE %s SCALAR SCRIPT available_packages.test_import_of_package() returns VARCHAR(2000000) AS
+def run_python_package_import_test(test_case, schema, language_alias, pkg, language_alias, fail=False, alternative=None):
+    sql=textwrap.dedent(f'''
+        CREATE OR REPLACE {language_alias} SCALAR SCRIPT {schema}.test_import_of_package() returns VARCHAR(2000000) AS
         
         def run(ctx):
             try:
-                import %s
+                import {pkg}
             except Exception as e:
                 import traceback
                 return traceback.format_exc()
             return None
         /
-        ''' % (language_alias, pkg))
+        ''')
     print(sql)
     test_case.query(sql)
     try:
-        rows = test_case.query('''SELECT available_packages.test_import_of_package() FROM dual''')
+        rows = test_case.query(f'''SELECT {schema}.test_import_of_package() FROM dual''')
         if not fail:
             test_case.assertRowsEqual([(None,)], rows)
         else:
--- tests/udf/available_python_packages_utils_test.py
+++ tests/udf/available_python_packages_utils_test.py
@@ -1,8 +1,7 @@
-import io
-
 from exasol_python_test_framework import udf, docker_db_environment
 from exasol_python_test_framework.udf.available_python_packages_utils import run_python_package_import_test
 
+import unittest
 
 class AvailablePackageTest(udf.TestCase):
     SCHEMA = "AvailablePackageTest"
@@ -12,13 +11,17 @@ class AvailablePackageTest(udf.TestCase):
         self.query(f'OPEN SCHEMA {self.SCHEMA}', ignore_errors=True)
 
     def test_package_available(self):
-        run_python_package_import_test(self, "pandas", "PYTHON3", fail=False, alternative=None)
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "pandas", fail=False, alternative=None)
 
-    def test_package_not_available(self):
-        run_python_package_import_test(self, "asdgkdbalgf", "PYTHON3", fail=True, alternative=None)
+    def test_package_not_available_expected(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "asdgkdbalgf", fail=True, alternative=None)
+
+    @unittest.expectedFailure
+    def test_package_not_available_not_expected(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "asdgkdbalgf", fail=False, alternative=None)
 
     def test_package_not_available_alternative(self):
-        run_python_package_import_test(self, "asdgkdbalgf", "PYTHON3", fail=False, alternative="pandas")
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "asdgkdbalgf", fail=False, alternative="pandas")
 
 if __name__ == '__main__':
     udf.main()
