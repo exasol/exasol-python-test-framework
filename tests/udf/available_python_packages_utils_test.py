1bee820282a2792fcd0dbd8e0e2a162018dab788
Torsten Kilias <tkilias@users.noreply.github.com>
2022-05-04 11:44:08 +0200

    #34: Add available python packages checks for UDFs to this package (#35)
    * Add available_python_packages_utils
    * Removed print and replaced python assert with test_case.fail
    
    Co-authored-by: Thomas Ubensee <34603111+tomuben@users.noreply.github.com>

---
+++ tests/udf/available_python_packages_utils_test.py
@@ -0,0 +1,30 @@
+from exasol_python_test_framework import udf, docker_db_environment
+from exasol_python_test_framework.udf.available_python_packages_utils import run_python_package_import_test
+
+import unittest
+
+class AvailablePackageTest(udf.TestCase):
+    SCHEMA = "AvailablePackageTest"
+
+    def setUp(self):
+        self.query(f'CREATE SCHEMA {self.SCHEMA}', ignore_errors=True)
+        self.query(f'OPEN SCHEMA {self.SCHEMA}', ignore_errors=True)
+
+    def test_package_available(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "pandas", fail=False, alternative=None)
+
+    def test_package_not_available_expected(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "NOT_INSTALLED_PACKAGE", fail=True, alternative=None)
+
+    @unittest.expectedFailure
+    def test_package_not_available_not_expected(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "NOT_INSTALLED_PACKAGE", fail=False, alternative=None)
+
+    def test_package_not_available_alternative(self):
+        run_python_package_import_test(self, self.SCHEMA, "PYTHON3", "NOT_INSTALLED_PACKAGE", fail=False, alternative="pandas")
+
+if __name__ == '__main__':
+    udf.main()
+
+
+
