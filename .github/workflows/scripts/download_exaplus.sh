157cc0d8fed75e61e4fdf91866956ab4a3382c41
Nicola Coretti <nicola.coretti@exasol.com>
2023-02-20 15:38:09 +0100

    Add URL's/references to compatible driver versions

--- .github/workflows/scripts/download_exaplus.sh
+++ .github/workflows/scripts/download_exaplus.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p "$1/downloads/EXAPLUS"
-curl -s https://x-up.s3.amazonaws.com/7.x/7.1.17/EXAplus-7.1.17.tar.gz | tar -C "$1/downloads/EXAPLUS" --strip-components 1 -zxf -
+curl -s https://exasol-script-languages-dependencies.s3.eu-central-1.amazonaws.com/EXAplus-7.0.11.tar.gz | tar -C "$1/downloads/EXAPLUS" --strip-components 1 -zxf -
--- .github/workflows/scripts/download_odbc_driver.sh
+++ .github/workflows/scripts/download_odbc_driver.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p "$1/downloads/ODBC"
-curl -s https://x-up.s3.amazonaws.com/7.x/7.1.17/EXASOL_ODBC-7.1.17.tar.gz | tar -C "$1/downloads/ODBC" --strip-components 1 -zxf -
+curl -s https://exasol-script-languages-dependencies.s3.eu-central-1.amazonaws.com/EXASOL_ODBC-7.0.11.tar.gz | tar -C "$1/downloads/ODBC" --strip-components 1 -zxf -
