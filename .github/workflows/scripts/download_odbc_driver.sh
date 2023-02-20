c05a57ff1724b4758fc5c243d3efac8d6c070357
Nicola Coretti <nicola.coretti@exasol.com>
2023-02-20 10:56:36 +0100

    Fix download url to odbc driver

--- .github/workflows/scripts/download_odbc_driver.sh
+++ .github/workflows/scripts/download_odbc_driver.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p "$1/downloads/ODBC"
-curl -s https://www.exasol.com/support/secure/attachment/155337/EXASOL_ODBC-7.0.11.tar.gz| tar -C "$1/downloads/ODBC" --strip-components 1 -zxf -
+curl -s https://x-up.s3.amazonaws.com/7.x/7.1.17/EXASOL_ODBC-7.1.17.tar.gz | tar -C "$1/downloads/ODBC" --strip-components 1 -zxf -
