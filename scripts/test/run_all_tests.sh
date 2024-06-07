6bee0ac51822165071a1eb1b2b5d1738eb020d06
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-06-07 09:21:05 -0300

    1. simplified CI + Release workflow 2. Added new test to CI

--- scripts/test/run_all_tests.sh
+++ scripts/test/run_all_tests.sh
@@ -2,20 +2,9 @@
    
 COMMAND_LINE_ARGS=("${@}") 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" 
- 
-source "$SCRIPT_DIR/../build/poetry_utils.sh"
-
-check_requirements
 
 set -euo pipefail
 
-init_poetry
 
-if [ -n "$POETRY_BIN" ]
-then
-  PYTHONPATH=. $POETRY_BIN run $SCRIPT_DIR/run_locally.sh --server "$1" --odbc-driver="$2"
-else
-  echo "Could not find poetry!"
-  exit 1
-fi
+
 
