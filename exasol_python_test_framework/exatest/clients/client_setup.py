02819347b4eba20676b2eb0bf3b0646f1e8829b1
ckunki <christoph.kuhnke@exasol.com>
2024-07-05 08:22:52 +0200

    #66: Added support for fingerprint in file `odbc.ini`

--- exasol_python_test_framework/exatest/clients/client_setup.py
+++ exasol_python_test_framework/exatest/clients/client_setup.py
@@ -3,15 +3,18 @@ import os
 import socket
 
 
+from inspect import cleandoc
+from pathlib import Path
+
+
 def validate_driver_path(arg: str):
     if not arg:
         raise argparse.ArgumentTypeError("The driver path is empty!")
-    import os.path
-    absolute = os.path.abspath(os.path.expanduser(arg))
-    if not os.path.exists(absolute):
-        raise argparse.ArgumentTypeError(f"The driver path '{absolute}' does not exist!")
+    path = Path(arg).expanduser().absolute()
+    if not path.exists():
+        raise argparse.ArgumentTypeError(f'The driver path "{path}" does not exist!')
     else:
-        return absolute
+        return path
 
 
 def validate_server(arg: str):
@@ -20,52 +23,97 @@ def validate_server(arg: str):
     return arg
 
 
+LOG_LEVELS = {
+    "off": None,
+    "error": "ON ERROR ONLY",
+    "normal": "DEFAULT",
+    "verbose": "VERBOSE",
+}
+
+ENV_VAR = "ODBCINI"
+
+
 class ClientSetup(object):
 
     def __init__(self):
         self.dsn = "exatest"
 
-    def odbc_arguments(self, arg_parser):
-        arg_parser.add_argument('--server',
-                          help='connection string',
-                          required=True,
-                          type=lambda x: validate_server(x))
-        arg_parser.add_argument('--user', help='connection user', nargs="?", type=str, default="sys")
-        arg_parser.add_argument('--password', help='connection password', nargs="?", type=str, default="exasol")
-        arg_parser.add_argument('--driver',
-                          help='path to ODBC driver',
-                          required=True,
-                          type=lambda x: validate_driver_path(x))
-        odbcloglevel = ('off', 'error', 'normal', 'verbose')
-        arg_parser.add_argument('--odbc-log', choices=odbcloglevel,
-                          help='activate ODBC driver log (default: %(default)s)')
+    def odbc_arguments(self, parser):
+        parser.add_argument(
+            "--server",
+            help="connection string",
+            required=True,
+            type=validate_server)
+        parser.add_argument(
+            "--user", help="connection user", nargs="?", type=str, default="sys")
+        parser.add_argument(
+            "--password", help="connection password", nargs="?", type=str, default="exasol")
+        parser.add_argument(
+            "--driver",
+            help="path to ODBC driver",
+            required=True,
+            type=validate_driver_path)
+        parser.add_argument(
+            "--odbc-log", choices=LOG_LEVELS,
+            help="activate ODBC driver log (default: off)")
+        return parser
 
-        return arg_parser
+    def _write_odbcini(self, log_path, server, driver, user,
+                       password, odbc_log, fingerprint = None) -> Path:
+        def cleandoc_nl(string):
+            return cleandoc(string) + "\n"
 
-    def _write_odbcini(self, log_path, server, driver, user, password, odbc_log):
-        name = os.path.realpath(os.path.join(log_path, 'odbc.ini'))
-        server=socket.getfqdn(server)
-        with open(name, 'w') as tmp:
-            tmp.write('[ODBC Data Sources]\n')
-            tmp.write('%s=EXASolution\n'%self.dsn)
-            tmp.write('\n')
-            tmp.write('[%s]\n'%self.dsn)
-            tmp.write('Driver = %s\n' % driver)
-            tmp.write('EXAHOST = %s\n' % server)
-            tmp.write('EXAUID = %s\n' % user)
-            tmp.write('EXAPWD = %s\n' % password)
-            tmp.write('CONNECTIONLCCTYPE = en_US.UTF-8\n')      # TODO Maybe make this optional
-            tmp.write('CONNECTIONLCNUMERIC = en_US.UTF-8\n')
-            tmp.write('SSLCERTIFICATE = SSL_VERIFY_NONE\n')
-            if odbc_log != 'off':
-                tmp.write('EXALOGFILE = %s/exaodbc.log\n' % log_path.logdir)
-                tmp.write('LOGMODE = %s\n' % {
-                        'error': 'ON ERROR ONLY',
-                        'normal': 'DEFAULT',
-                        'verbose': 'VERBOSE',
-                        }[odbc_log])
-        return name
+        path = Path(log_path).joinpath("odbc.ini").resolve()
+        server = socket.getfqdn(server)
+        with path.open('w') as file:
+            file.write(cleandoc_nl(
+                    # TODO: Maybe make CONNECTIONLCCTYPE optional
+                    f"""
+                    [ODBC Data Sources]
+                    {self.dsn}=EXASolution
 
-    def prepare_odbc_init(self, log_path, server, driver, user, password, odbc_log):
-        name = self._write_odbcini(log_path, server, driver, user, password, odbc_log)
-        os.environ['ODBCINI'] = name
+                    [{self.dsn}]
+                    Driver = {driver}
+                    EXAHOST = {server}
+                    EXAUID = {user}
+                    EXAPWD = {password}
+                    CONNECTIONLCCTYPE = en_US.UTF-8
+                    CONNECTIONLCNUMERIC = en_US.UTF-8
+                    SSLCERTIFICATE = SSL_VERIFY_NONE
+                    """
+                    ))
+            if fingerprint:
+                file.write(f"FINGERPRINT = {fingerprint}\n")
+            if odbc_log != "off":
+                file.write(cleandoc_nl(
+                        f"""
+                        EXALOGFILE = {log_path}/exaodbc.log
+                        LOG_LEVELS = {LOG_LEVELS[odbc_log]}
+                        """
+                        ))
+        return path
+
+    def prepare_odbc_init(self, log_path, server, driver, user, password, odbc_log, fingerprint = None):
+        path = self._write_odbcini(log_path, server, driver, user, password, odbc_log, fingerprint)
+        os.environ[ENV_VAR] = path
+
+
+import sys
+if __name__ == "__main__":
+    setup = ClientSetup()
+    parser = argparse.ArgumentParser(
+        description="""
+        Synchronize sub-tasks from GitHub to Jira.
+        """
+    )
+    args = setup.odbc_arguments(parser).parse_args()
+    dir = Path("/home/chku/tmp")
+    setup.prepare_odbc_init(
+        dir,
+        "localhost",
+        "my-driver",
+        "my-user",
+        "my-password",
+        "verbose",
+        "fp",
+    )
