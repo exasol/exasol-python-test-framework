514525537903c8357d66e46cba9a886bd9eff337
Ariel Schulz <43442541+ArBridgeman@users.noreply.github.com>
2026-06-02 08:21:00 +0200

    Feature/101 add ptb 8.1.1 (#108)
    * Add PTB 8.1.1
    * Use uvx migrate-to-uv to update to be more PEP-compliant
    * Add additional values to pyproject.toml for PTB
    * Add nox files
    * Add pre-commit hook
    * Replace release with PTB variant
    * Start .workflow-patcher.yml
    * Update .gitignore
    * Add version info
    * Switch main.yml to slow-checks.yml for PTB
    * Add PTB workflows
    * Add smoke test
    * Add changelog entry and convert to unreleased.md
    * Add .gitattributes
    * Restrict python versions
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>

---
+++ noxconfig.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from pathlib import Path
+from pydantic import computed_field
+
+from exasol.toolbox.config import BaseConfig
+
+
+class Config(BaseConfig):
+    @computed_field  # type: ignore[misc]
+    @property
+    def source_code_path(self) -> Path:
+        """
+        Path to the source code of the project.
+
+        This needs to be overridden due to a custom directory setup. This will be
+        addressed in:
+            https://github.com/exasol/exasol-python-test-framework/issues/106
+        """
+        return self.root_path / self.project_name
+
+PROJECT_CONFIG = Config(
+    project_name="exasol_python_test_framework",
+    root_path=Path(__file__).parent,
+    python_versions=("3.10", "3.11"),
+)
---
+++ test/unit/smoke_test.py
@@ -0,0 +1,5 @@
+import exasol_python_test_framework # noqa: F401, allowed as for smoke test
+
+
+def test_unit_smoke_test():
+    assert True
